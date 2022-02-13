e0cd0c096231d235771c58f7f7b3e5a1325ab3b9
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-02-13 11:46:24 +0700

    first commit

---
+++ KKM/app/src/main/java/com/example/kkm/ui/SplashscreenActivity.kt
@@ -0,0 +1,12 @@
+package com.example.kkm.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.kkm.R
+
+class SplashscreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_slashscreen)
+    }
+}

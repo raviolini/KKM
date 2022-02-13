9724e44ed9721918ca76d59df3573fabe1c438f9
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-02-13 15:11:33 +0700

    added new resource

---
+++ KKM/app/src/main/java/com/example/kkm/data/kos/Item.kt
@@ -0,0 +1,10 @@
+package com.example.kkm.data.kos
+
+data class Item(
+    val id : Int,
+    val nama : String,
+    val alamat : String,
+    val imageUrl : String,
+    val tipe : String,
+    val status : String,
+)

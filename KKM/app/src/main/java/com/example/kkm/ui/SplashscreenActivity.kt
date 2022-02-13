567e23c0055cc50bb090084de85c4e97e7d26ce1
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-02-13 16:12:10 +0700

    added images in splashscreen

--- KKM/app/src/main/java/com/example/kkm/ui/SplashscreenActivity.kt
+++ KKM/app/src/main/java/com/example/kkm/ui/SplashscreenActivity.kt
@@ -1,10 +1,15 @@
 package com.example.kkm.ui
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.window.SplashScreen
 import com.example.kkm.R
+import com.example.kkm.databinding.ActivitySlashscreenBinding
 
+@SuppressLint("CustomSplashScreen")
 class SplashscreenActivity : AppCompatActivity() {
+    private lateinit var SplashScreenBinding : ActivitySlashscreenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_slashscreen)

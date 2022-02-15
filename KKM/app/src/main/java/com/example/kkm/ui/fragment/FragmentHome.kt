25e286cc5f4bbfed54565b18c685ef9f4d9961ad
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-02-16 03:06:13 +0700

    LETSGOO FRAGMENT HOME, still resolving for that FragmentHomeDirections tho

--- KKM/app/src/main/java/com/example/kkm/ui/fragment/FragmentHome.kt
+++ KKM/app/src/main/java/com/example/kkm/ui/fragment/FragmentHome.kt
@@ -1,7 +1,39 @@
 package com.example.kkm.ui.fragment
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.navGraphViewModels
+import com.example.kkm.R
+import com.example.kkm.databinding.FragmentHomeBinding
 import com.example.kkm.misc.ShowStates
+import com.example.kkm.ui.adapter.ItemAdapter
+import com.example.kkm.ui.vm.HomeVM
 
 class FragmentHome  : Fragment(), ShowStates{
+    private lateinit var bindingHome : FragmentHomeBinding
+    private lateinit var homeAdapter : ItemAdapter
+    private val homeVM : HomeVM by navGraphViewModels(R.id.my_nav)
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        bindingHome = FragmentHomeBinding.inflate(layoutInflater, container, false)
+        return bindingHome.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setHasOptionsMenu(true)
+        bindingHome.errorLayout.emptyText.text = getString(R.string.search_for_kos)
+
+        /*homeAdapter = ItemAdapter(arrayListOf(){name, iv->
+            findNavController().navigate(FragmentHomeDirections)
+        })*/
+    }
 }

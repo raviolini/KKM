95f0466ea18dc1a21ea194decb9cb1c4d9f2487a
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-02-16 18:40:12 +0700

    added myglideapp and fixed no history in manifest

--- KKM/app/src/main/java/com/example/kkm/binding/AvatarBindingAdapter.kt
+++ KKM/app/src/main/java/com/example/kkm/binding/AvatarBindingAdapter.kt
@@ -1,16 +1,15 @@
 package com.example.kkm.binding
 
-import android.app.DownloadManager
+
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
-import com.bumptech.glide.Glide
-import com.bumptech.glide.request.RequestOptions
 import com.bumptech.glide.request.RequestOptions.circleCropTransform
 import com.example.kkm.R
+import com.example.kkm.misc.GlideApp
 
-@BindingAdapter("imageUrl")
-fun imageKost(imageView : ImageView, img : String)=
-    Glide.with(imageView.context)
-        .load(img)
-        .apply(RequestOptions.circleCropTransform().placeholder(R.drawable.ravioli))
+@BindingAdapter("avatar")
+fun avatar(imageView : ImageView, avatar : String)=
+    GlideApp.with(imageView.context)
+        .load(avatar)
+        .apply(circleCropTransform().placeholder(R.drawable.ravioli))
         .into(imageView)

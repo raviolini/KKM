6d2828310f32fd16310dfd4cc5169621228aa982
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-02-17 07:25:17 +0700

    added some code in fragment details part 2 still waiting for noval

--- KKM/app/src/main/java/com/example/kkm/ui/fragment/FragmentDetail.kt
+++ KKM/app/src/main/java/com/example/kkm/ui/fragment/FragmentDetail.kt
@@ -1,6 +1,11 @@
 package com.example.kkm.ui.fragment
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.navArgs
 import androidx.viewpager.widget.PagerAdapter
 import com.example.kkm.data.kos.Item
@@ -14,4 +19,22 @@ class FragmentDetail : Fragment(), ShowStates {
     private lateinit var detailVM: DetailVM
     private lateinit var item : Item
     private val args : FragmentDetailArgs by navArgs()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        detailVM = ViewModelProvider(
+            this
+        )[DetailVM::class.java]
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        bindingDetail = FragmentDetailBinding.inflate(layoutInflater, container, false)
+        //bindingDetail.lifecycleOwner = viewLifecycleOwner
+
+        return bindingDetail.root
+    }
 }

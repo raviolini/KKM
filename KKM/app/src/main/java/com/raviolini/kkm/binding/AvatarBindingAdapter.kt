5f0bb2eb01fb8563f4af2e8ee6cb91b439d71f64
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-03-02 13:21:45 +0700

    fixed import

--- KKM/app/src/main/java/com/raviolini/kkm/binding/AvatarBindingAdapter.kt
+++ KKM/app/src/main/java/com/raviolini/kkm/binding/AvatarBindingAdapter.kt
@@ -4,7 +4,7 @@ package com.raviolini.kkm.binding
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.request.RequestOptions.circleCropTransform
-import com.raviolini.kkm.R
+import com.example.kkm.R
 import com.raviolini.kkm.misc.GlideApp
 
 @BindingAdapter("avatar")

a936345d3cb6e2a48c248c3a18bb4e04159271ad
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-03-31 07:35:35 +0700

    reopening the project

--- KKM/app/src/main/java/com/raviolini/kkm/misc/MyStates.kt
+++
@@ -1,7 +0,0 @@
-package com.raviolini.kkm.misc
-
-enum class MyStates {
-    IS_SUCCESS,
-    IS_ERROR,
-    IS_LOADING
-}
--- KKM/app/src/main/java/com/raviolini/kkm/misc/ResourceStats.kt
+++
@@ -1,9 +0,0 @@
-package com.raviolini.kkm.misc
-
-class ResourceStats<out E>(val states : MyStates, val data : E?, val message : String?) {
-    companion object{
-        fun <E> onLoading(data : E?) : ResourceStats<E> = ResourceStats(MyStates.IS_LOADING, data, null)
-        fun <E> onSuccess(data : E) : ResourceStats<E> = ResourceStats(MyStates.IS_SUCCESS, data, null)
-        fun <E> onError(data:E?, message: String) : ResourceStats<E> = ResourceStats(MyStates.IS_ERROR, data, message)
-    }
-}
